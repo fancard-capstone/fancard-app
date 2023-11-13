ae5d026434ac956a270f054ae5f7927475e642bc
Manoj Sharma <manojs74930@gmail.com>
2023-11-13 12:49:37 -0500

    changes

--- lib/profile.dart
+++ lib/profile.dart
@@ -10,78 +10,34 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: UserDetailsWidget(),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Color(0xFFFFCCCC),
+          body: Center(
+            child: Image.asset(
+              'assets/user_profile.jpg',
+              height: 250,
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Buttons(),
+          )),
     );
   }
 }
 
-//somrethying
-//testing
-class UserDetailsWidget extends StatelessWidget {
-  const UserDetailsWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Circular image at the top left
-          Container(
-            width: 80.0,
-            height: 80.0,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage('assets/user_image.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          // Box with user details
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Name: Manoj Sharma'),
-                Text('Student ID: 1111111'),
-                Text('Issued On: 2022-05-01'),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16.0),
-          // Circular icon with "NFC" at the top right
-          Positioned(
-            top: 0,
-            right: 0,
-            child: Container(
-              width: 40.0,
-              height: 40.0,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
-              child: const Center(
-                child: Text(
-                  'NFC',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+// ignore: non_constant_identifier_names
+Buttons() {
+  TextButton(
+    style: ButtonStyle(
+      overlayColor: MaterialStateProperty.resolveWith<Color?>(
+          (Set<MaterialState> states) {
+        if (states.contains(MaterialState.focused))
+          return const Color.fromARGB(255, 57, 54, 244);
+        return null; // Defer to the widget's default.
+      }),
+    ),
+    onPressed: () {},
+    child: const Text('TextButton'),
+  );
 }
